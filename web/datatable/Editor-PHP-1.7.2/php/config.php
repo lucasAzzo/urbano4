c927c00242f871a674c81ae5b87f54e2f2abdb04
aledaas <aledaas@gmail.com>
2018-03-15 15:56:58 -0300

    breadcrumbs - inicio

--- web/datatable/Editor-PHP-1.7.2/php/config.php
+++ web/datatable/Editor-PHP-1.7.2/php/config.php
@@ -9,12 +9,12 @@ ini_set('display_errors', '1');
  * Database user / pass
  */
 $sql_details = array(
-	"type" => "Mysql",   // Database type: "Mysql", "Postgres", "Sqlserver", "Sqlite" or "Oracle"
-	"user" => "",        // Database user name
-	"pass" => "",        // Database password
-	"host" => "",        // Database host
-	"port" => "",        // Database connection port (can be left empty for default)
-	"db"   => "",        // Database name
+	"type" => "Postgres",   // Database type: "Mysql", "Postgres", "Sqlserver", "Sqlite" or "Oracle"
+	"user" => "postgres",        // Database user name
+	"pass" => "Urb4n0*#",        // Database password
+	"host" => "urbanotesting.cxyyzovbwtwj.us-east-2.rds.amazonaws.com",        // Database host
+	"port" => "5432",        // Database connection port (can be left empty for default)
+	"db"   => "datatable",        // Database name
 	"dsn"  => "",        // PHP DSN extra information. Set as `charset=utf8` if you are using MySQL
 	"pdoAttr" => array() // PHP PDO attributes array. See the PHP documentation for all options
 );
