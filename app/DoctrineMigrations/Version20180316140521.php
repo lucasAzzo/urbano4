d812ca0e5ea9946f8d87ceb42aaf61439a3fcee0
Lucas <lucas_vm_11@hotmail.com>
2018-03-16 11:28:05 -0300

    commit migration

--- app/DoctrineMigrations/Version20180316140521.php
+++ app/DoctrineMigrations/Version20180316140521.php
@@ -18,15 +18,15 @@ class Version20180316140521 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('DROP SEQUENCE sub_producto_id_seq CASCADE');
+        //$this->addSql('DROP SEQUENCE sub_producto_id_seq CASCADE');
         $this->addSql('CREATE TABLE menus_roles (id_menu INT NOT NULL, id_role INT NOT NULL, PRIMARY KEY(id_menu, id_role))');
         $this->addSql('CREATE INDEX IDX_E75CC96BF6252691 ON menus_roles (id_menu)');
         $this->addSql('CREATE INDEX IDX_E75CC96BDC499668 ON menus_roles (id_role)');
         $this->addSql('ALTER TABLE menus_roles ADD CONSTRAINT FK_E75CC96BF6252691 FOREIGN KEY (id_menu) REFERENCES menu (id_menu) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE menus_roles ADD CONSTRAINT FK_E75CC96BDC499668 FOREIGN KEY (id_role) REFERENCES roles (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('DROP INDEX uniq_a7bb061520332d99');
-        $this->addSql('ALTER TABLE producto DROP codigo');
-        $this->addSql('ALTER TABLE upload ALTER upload_timestamp SET DEFAULT CURRENT_TIMESTAMP');
+        //$this->addSql('DROP INDEX uniq_a7bb061520332d99');
+        //$this->addSql('ALTER TABLE producto DROP codigo');
+        //$this->addSql('ALTER TABLE upload ALTER upload_timestamp SET DEFAULT CURRENT_TIMESTAMP');
     }
 
     /**
