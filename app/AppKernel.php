f46d6d3f73f8f8f6ebe1a68959bf0f5c35327bf3
aledaas <aledaas@gmail.com>
2018-03-19 14:51:37 -0300

    bundles - enviroment dev

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,12 +16,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-
-            new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle(),
-            new Sensio\Bundle\DistributionBundle\SensioDistributionBundle(),
-            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
-
-
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
