f040bf71230649b37d4cadeae265f57121df60b7
Samuel Gonzalez <samueldgt@gmail.com>
2018-04-16 15:49:16 -0300

    Creation of canalization process

---
+++ src/AppBundle/Entity/CodigoPostal.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * CodigoPostal
+ *
+ * @ORM\Table(name="codigo_postal")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CodigoPostalRepository")
+ */
+class CodigoPostal
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="codPostal", type="integer")
+     */
+    private $codPostal;
+
+    /**
+     * @var \AppBundle\Entity\Sucursal
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Sucursal")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="id_sucursal", referencedColumnName="id")
+     * })
+     */
+    private $idSucursal;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created", type="date")
+     */
+    private $created;
+
+    /**
+     * @var \AppBundle\Entity\User
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="id_user", referencedColumnName="id")
+     * })
+     */
+    private $idUser;
+
+    /**
+     * @var \AppBundle\Entity\Estado
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Estado")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="id_estado", referencedColumnName="id")
+     * })
+     */
+    private $idEstado;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set codPostal
+     *
+     * @param integer $codPostal
+     *
+     * @return CodigoPostal
+     */
+    public function setCodPostal($codPostal)
+    {
+        $this->codPostal = $codPostal;
+
+        return $this;
+    }
+
+    /**
+     * Get codPostal
+     *
+     * @return int
+     */
+    public function getCodPostal()
+    {
+        return $this->codPostal;
+    }
+
+    /**
+     * Set idSucursal
+     *
+     * @param integer $idSucursal
+     *
+     * @return CodigoPostal
+     */
+    public function setIdSucursal(\AppBundle\Entity\Sucursal $idSucursal)
+    {
+        $this->idSucursal = $idSucursal;
+
+        return $this;
+    }
+
+    /**
+     * Get idSucursal
+     *
+     * @return int
+     */
+    public function getIdSucursal()
+    {
+        return $this->idSucursal;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     *
+     * @return CodigoPostal
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set idUser
+     *
+     * @param integer $idUser
+     *
+     * @return CodigoPostal
+     */
+    public function setIdUser(\AppBundle\Entity\User $idUser)
+    {
+        $this->idUser = $idUser;
+
+        return $this;
+    }
+
+    /**
+     * Get idUser
+     *
+     * @return int
+     */
+    public function getIdUser()
+    {
+        return $this->idUser;
+    }
+
+    /**
+     * Set idEstado
+     *
+     * @param integer $idEstado
+     *
+     * @return CodigoPostal
+     */
+    public function setIdEstado(\AppBundle\Entity\Estado $idEstado)
+    {
+        $this->idEstado = $idEstado;
+
+        return $this;
+    }
+
+    /**
+     * Get idEstado
+     *
+     * @return int
+     */
+    public function getIdEstado()
+    {
+        return $this->idEstado;
+    }
+}
+
